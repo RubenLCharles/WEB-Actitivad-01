9d302cbbe9a901ad9baf547b6fa9c4333bad29d3
RubenLCharles <33010291+RubenLCharles@users.noreply.github.com>
2020-10-27 22:27:56 -0600

    About Me Page Added

--- script/script.js
+++ script/script.js
@@ -2,21 +2,49 @@
 function toggleEdu() {
     var exp = document.getElementById("exp");
     var edu = document.getElementById("edu");
+    var about = document.getElementById("aboutme"); 
         exp.style.display = "none";
-        edu.style.display = "block";  
+        edu.style.display = "block";
+        about.style.display = "none";
 }
 
 function toggleExp() {
     var exp = document.getElementById("exp");
     var edu = document.getElementById("edu");
+    var about = document.getElementById("aboutme");
         exp.style.display = "block";
         edu.style.display = "none";  
+        about.style.display = "none";
 }
 
+function toggleAbout() {
+    var exp = document.getElementById("exp");
+    var edu = document.getElementById("edu");
+    var about = document.getElementById("aboutme");
+        exp.style.display = "none";
+        edu.style.display = "none";  
+        about.style.display = "block";
+}
 
 function main(){
+
+    const MongoClient = require('mongodb').MongoClient;
+    const uri = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.mwisj.mongodb.net/sample_restaurants?retryWrites=true&w=majority";
+    
+    MongoClient.connect(uri, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("sample_restaurants");
+        dbo.collection("restaurants").findOne({}, function(err, result) {
+          if (err) throw err;
+          console.log(result.name);
+          db.close();
+        });
+      }); 
+
+    
     var aEdu = document.getElementById("aEdu");
     var aExp = document.getElementById("aExp");
+    var aBout = document.getElementById("aBout");
 
     aEdu.addEventListener(
         "click",
@@ -30,6 +58,14 @@ function main(){
         "click",
         function() { 
             toggleExp();
+        },
+        false
+    );
+
+    aBout.addEventListener (
+        "click",
+        function(){
+            toggleAbout();
         },
         false
     );
@@ -70,6 +106,18 @@ function main(){
             ]
         }
     })
+
+    var likes = new Vue({
+        el: '#likes',
+        data: {
+          items: [
+            { mensaje: 'VideoGames' },
+            { mensaje: 'Making Music' },
+            { mensaje: 'Producing Videos'},
+            { mensaje: 'Twitch Streaming'}
+          ]
+        }
+      })
 }
 
 
